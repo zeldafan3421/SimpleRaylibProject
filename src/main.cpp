e66bec569e73a68f4d16afd23041c455ea327738
Aiden Ross <aiden.r.ross24@gmail.com>
2025-12-08 22:40:58 -0500

    add global variables in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include "raylib.h"
 
+constexpr int WINDOW_WIDTH = 1280;
+constexpr int WINDOW_HEIGHT = 720;
+const char *WINDOW_TITLE = "Raylib project"; 
+constexpr Color BG_COLOR = WHITE;
+
 int main ()
 {
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	InitWindow(1280, 800, "Hello Raylib");
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
-		ClearBackground(BLACK);
+		ClearBackground(WHITE);
 		EndDrawing();
 	}
 
